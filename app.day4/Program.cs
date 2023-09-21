599dc8702a84bf2e401b5c9c75f23189a10c5f07
Maha <mahanubi829@gmail.com>
2023-09-21 15:17:59 +0300

    v2

---
+++ app.day4/Program.cs
@@ -0,0 +1,41 @@
+﻿using App;
+using System;
+namespace app {
+    public class program
+    {
+        static void Main(String[] args)
+        {
+
+            Console.WriteLine("how many trainees ");
+            int count =int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the number of Trainee");
+            //int traineeNo = int.Parse(Console.ReadLine());
+            Trainee[] trainees = new Trainee[count];
+            Arr arr = new Arr(count);
+            for (int i = 0; i < count; i++)
+            {
+                trainees[i] = new Trainee();
+                Console.WriteLine("Enter Name");
+                trainees[i].Name = Console.ReadLine();
+                Console.WriteLine("Enter Id");
+                trainees[i].Id = int.Parse(Console.ReadLine());
+                arr.Add(trainees[i]);
+               
+            }
+            foreach (Trainee trainee in arr)
+            {
+                Console.WriteLine(trainee);
+            }
+            arr.Remove(0);
+
+            for(int i =0; i < arr.Count -1; i++)
+                Console.WriteLine(arr[i]);
+
+
+
+        }
+    }
+}
+
+
+
